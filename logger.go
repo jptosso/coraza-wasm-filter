31438fbb8500c9fdd319cacdc42d0c45d15d4627
Anuraag Agrawal <anuraaga@gmail.com>
2022-08-31 11:27:45 +0900

    Implement debuglogger

---
+++ logger.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"io"
+
+	"github.com/corazawaf/coraza/v3"
+)
+
+type debugLogger struct {
+	level coraza.LogLevel
+}
+
+func (l *debugLogger) Info(message string, args ...interface{}) {
+	if l.level >= coraza.LogLevelInfo {
+		proxywasm.LogInfof(message, args...)
+	}
+}
+
+func (l *debugLogger) Warn(message string, args ...interface{}) {
+	if l.level >= coraza.LogLevelWarn {
+		proxywasm.LogWarnf(message, args...)
+	}
+}
+
+func (l *debugLogger) Error(message string, args ...interface{}) {
+	if l.level >= coraza.LogLevelError {
+		proxywasm.LogErrorf(message, args...)
+	}
+}
+
+func (l *debugLogger) Debug(message string, args ...interface{}) {
+	if l.level >= coraza.LogLevelDebug {
+		proxywasm.LogDebugf(message, args...)
+	}
+}
+
+func (l *debugLogger) Trace(message string, args ...interface{}) {
+	if l.level >= coraza.LogLevelTrace {
+		proxywasm.LogTracef(message, args...)
+	}
+}
+
+func (l *debugLogger) SetLevel(level coraza.LogLevel) {
+	l.level = level
+}
+
+func (l *debugLogger) SetOutput(w io.Writer) {
+	panic("not supported")
+}
